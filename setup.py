2c3920f2b100ccaf7ae1479cf76d2fdafe74d52a
John Mount <jmount@win-vector.com>
2018-11-18 10:39:55 -0800

    fix url

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='wvpy',
       version='0.1',
       author='John Mount',
       author_email='jmount@win-vector.com',
-      url='https://github.com/WinVector/vtreat',
+      url='https://github.com/WinVector/wvpy',
       packages=['wvpy'],
       install_requires=[
           'statistics',
