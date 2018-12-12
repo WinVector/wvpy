55d81ba0f8f3fa26653093b82fca0da368c1a245
John Mount <jmount@win-vector.com>
2018-12-12 09:44:36 -0500

    add plot

--- wvpy/util.py
+++ wvpy/util.py
@@ -119,6 +119,16 @@ def dual_hist_plot_proba1(probs, istrue):
     #g = g.map(seaborn.distplot, "prob", bins=bins)
     matplotlib.pyplot.show()
 
+def dual_hist_plot(probs, istrue):
+    """plot a dual histogram plot of numeric prediction probs[:,1] against boolean istrue"""
+    matplotlib.pyplot.gcf().clear()
+    pf = pandas.DataFrame({'prob' : [ probs[i] for i in range(probs.shape[0])], 'istrue' : istrue})
+    g = seaborn.FacetGrid(pf, row="istrue", height=4, aspect=3)
+    bins = numpy.arange(0, 1.1, 0.1)
+    g = g.map(matplotlib.pyplot.hist, "prob", bins=bins)
+    #g = g.map(seaborn.distplot, "prob", bins=bins)
+    matplotlib.pyplot.show()
+
 # https://stackoverflow.com/questions/5228158/cartesian-product-of-a-dictionary-of-lists
 def search_grid(inp):
     """build a cross product of all named dictionary entries"""
