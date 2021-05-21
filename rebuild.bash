33b262b03cc97adf294c62b3268683998a967b14
John Mount <jmount@win-vector.com>
2021-05-21 12:49:29 -0700

    try to rebuild TODO: find out why coverage is appearing as zero

--- rebuild.bash
+++ rebuild.bash
@@ -5,7 +5,7 @@ pip uninstall -y wvpy
 python3 setup.py sdist bdist_wheel
 pip install dist/wvpy-*.tar.gz
 popd
-pdoc -o docs pkg/vtreat
+pdoc -o docs pkg/wvpy
 pytest
 pytest --cov pkg/wvpy pkg/tests > coverage.txt
 cat coverage.txt
