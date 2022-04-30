ac7fad4234a47f7762c4b0c16419015869821163
John Mount <jmount@win-vector.com>
2022-04-30 14:42:19 -0700

    example of using renders and converters

---
+++ examples/worksheets/plot.py
@@ -0,0 +1,38 @@
+
+''' begin text
+Example of a Python Jupyter worksheet.
+'''  # end text
+
+# import our packages
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+''' begin text
+Makes some example data.
+'''  # end text
+
+d = pd.DataFrame({
+    'x': np.linspace(0, 7, num=100)
+})
+d['y'] = np.sin(d['x'])
+
+d
+
+''' begin text
+Plot our data.
+'''  # end text
+
+sns.lineplot(
+    data=d,
+    x='x',
+    y='y',
+)
+plt.title('example plot')
+plt.show()
+
+''' begin text
+And that is our example.
+'''  # end text
+
---
+++ examples/worksheets/plot_nbconvert.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Example of a Python Jupyter worksheet.
+
+# In[1]:
+
+
+# import our packages
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+# Makes some example data.
+
+# In[2]:
+
+
+d = pd.DataFrame({
+    'x': np.linspace(0, 7, num=100)
+})
+d['y'] = np.sin(d['x'])
+
+d
+
+
+# Plot our data.
+
+# In[4]:
+
+
+sns.lineplot(
+    data=d,
+    x='x',
+    y='y',
+)
+plt.title('example plot')
+plt.show()
+
+
+# And that is our example.
