5b5da4442bc634131bfa94ec3e36673bbba06b4b
John Mount <jmount@win-vector.com>
2022-05-04 12:53:24 -0700

    bump version

--- pkg/build/lib/wvpy/__init__.py
+++ pkg/build/lib/wvpy/__init__.py
@@ -1,5 +1,5 @@
 __docformat__ = "restructuredtext"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 __doc__ = """
 This<https://github.com/WinVector/wvpy> is a package of example files for teaching data science.
--- pkg/setup.py
+++ pkg/setup.py
@@ -8,7 +8,7 @@ Simple utilities for teaching Pandas, Jupyter, seaborn, and sklearn.
 
 setuptools.setup(
     name="wvpy",
-    version="0.3.1",
+    version="0.3.2",
     author="John Mount",
     author_email="jmount@win-vector.com",
     url="https://github.com/WinVector/wvpy",
