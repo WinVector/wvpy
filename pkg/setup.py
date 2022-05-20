c95ef8006b0aabf787fbc7c9cbe9e51e96af42a8
John Mount <jmount@win-vector.com>
2022-05-20 15:19:11 -0700

    bump version

--- pkg/setup.py
+++ pkg/setup.py
@@ -8,7 +8,7 @@ Simple utilities for teaching Pandas, Jupyter, seaborn, and sklearn.
 
 setuptools.setup(
     name="wvpy",
-    version="0.3.2",
+    version="0.3.3",
     author="John Mount",
     author_email="jmount@win-vector.com",
     url="https://github.com/WinVector/wvpy",
