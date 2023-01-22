9a8573ffeea480cf734dfb161fe46539b6ea6fe7
John Mount <jmount@win-vector.com>
2023-01-21 16:58:52 -0800

    clean up script a bit

--- set_up_dev_env.bash
+++ set_up_dev_env.bash
@@ -1,8 +1,8 @@
-
+#!/bin/bash
 
 conda remove --name wvpy_dev_env --all --yes
 conda env create -f wvpy_dev_env.yaml
 conda activate wvpy_dev_env
-pip install --no-deps -e "$(pwd)/pkg"  # sym link to source files
+pip install --no-deps -e "$(pwd)/pkg"
 
 
