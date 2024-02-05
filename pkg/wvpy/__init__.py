48cf39944d4c2924eb484ac96390431548e3b15e
John Mount <jmount@win-vector.com>
2024-02-05 10:15:41 -0800

    bump version post release

--- pkg/wvpy/__init__.py
+++ pkg/wvpy/__init__.py
@@ -1,5 +1,5 @@
 __docformat__ = "restructuredtext"
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 __doc__ = """
 <https://github.com/WinVector/wvpy> a system for converting and rendering Jupyter notebooks.
