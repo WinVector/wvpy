c3cfde0db7538507efb1979f68f8fa3ff1c3d8df
John Mount <jmount@win-vector.com>
2024-02-06 09:55:32 -0800

    switch to for-loop

--- examples/weather/run_weather_reports_1.py
+++ examples/weather/run_weather_reports_1.py
@@ -1,20 +1,13 @@
 
 # python run_weather_reports_1.py
-from wvpy.jtools import JTask, run_pool
+from wvpy.jtools import JTask
 
 
 if __name__ == '__main__':
-    state_codes = [
-        "CA",
-        "TX",
-        "KS",
-    ]
-    tasks = [
-        JTask(
+    for state_code in ["CA", "TX", "KS"]:
+        task = JTask(
             sheet_name="weather_example_1.ipynb",
             sheet_vars={"state_code": state_code},
             output_suffix=f"_{state_code}",
         )
-        for state_code in state_codes
-    ]
-    run_pool(tasks)
+        task.render_as_html()
